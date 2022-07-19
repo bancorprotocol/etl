58474b7cd485c3bcdc0ea8bcf47f4a9f7108699c
mikewcasale <mike.w.casale@gmail.com>
2022-07-18 20:06:18 -0700

    removing package structure

--- build/lib/bancor_etl/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-# --------------------------------------------------------------------------------------------------------------------
-# Licensed under the Bprotocol Foundation (Bancor) LICENSE. See License.txt in the project root for license information.
-# --------------------------------------------------------------------------------------------------------------------
-"""Spec, simulation, & emulation modules for Bancor v3."""
-
-__version__ = "0.0.1"
